25192306942a7bf75da36a808b5ef782202964f4
Disha Vyas <dishavyas263@gmail.com>
2023-03-14 01:28:32 +0530

    first commit

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -43,12 +43,12 @@ function Home2() {
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  Modern Javascript Library and Framework
                 </b>
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> React.js </b>
               </i>
             </p>
           </Col>
